c6013759a3ba2f5ae3d9e8a0ed14c358aa7164a6
mdu <mdu@DESKTOP-MS4GQTV>
2020-07-17 18:49:16 +0845

    new

---
+++ src/silnd201626829/Silnd201626829.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package silnd201626829;
+
+/**
+ *
+ * @author mdu
+ */
+public class Silnd201626829 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
